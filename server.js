a0ef61dd028e4f1b14343e242f3e302c82e45311
mhgit1 <mkkha7@gmail.com>
2016-01-04 14:45:59 +0200

    update

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ var app = express();
 //You need to define theses two variables with these
 //two environment variables to get your app working in openshift
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;           //process.env.PORT oletus portti 80 (yleensä varattu)  ||(tai) 
+                                                                                        // process.env.OPENSHIFT_NODEJS_PORT, tai 8080, jos muut ei käy
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP   || '0.0.0.0';      //Onko prosessimuuttujassa, openshiftin ympäristön ip, 
+                                                                                        //tai default oman koneen verkkokortin ip
 
 //When this server is connected, return the index.html as response
 app.get('/',function(req,res){
